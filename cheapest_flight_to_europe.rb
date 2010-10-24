16e7a0429e12f02b9fb9d443fd6cd472d5167b65
Douglas Sellers <douglas.sellers@gmail.com>
2010-10-23 22:33:44 -0700

    Made it so that now the cheapest flights to europe

--- cheapest_flight_to_europe.rb
+++ cheapest_flight_to_europe.rb
@@ -9,19 +9,74 @@ else
   start_date = ARGV[0]
   end_date = ARGV[1]
   num_travelers = ARGV[2]
+
+  airport_codes = [
+                   [ "Vienna", "Austria", "VIE" ],
+                   [ "Brussels", "Belgium", "BRU" ],
+                   [ "Prague", "Czech Republic", "PRG"],
+                   [ "Copenhagen", "Denmark", "CPH" ],
+                   [ "Birmingham", "England", "BHX" ],
+                   [ "Istanbul", "Turkey", "IST" ],
+                   [ "Zurich", "Switzerland", "ZRH" ],
+                   [ "Geneva", "Switzerland", "GVA" ],
+                   [ "Stockholm", "Sweden", "ARN"],
+                   [ "Madrid", "Spain", "MAD" ],
+                   [ "Barcelona", "Spain", "BCN" ],
+                   [ "Glasgow", "Scotland", "GLA" ],
+                   [ "Bucharest", "Romania", "OTP" ],
+                   [ "Lisbon", "Portugal", "LIS" ],
+                   [ "Warsaw", "Poland", "WAW" ],
+                   [ "Oslo", "Norway", "OSL" ],
+                   [ "Amsterdam", "Netherlands", "AMS" ],
+                   [ "Luxembourg", "Luxembourg", "LUX" ],
+                   [ "Venice", "Italy", "VCE" ],
+                   [ "Rome", "Italy", "FCO" ],
+                   [ "Milan", "Italy", "MXP"],
+                   [ "Florence", "Italy", "FLR" ],
+                   [ "Bologna", "Italy", "BLQ" ],
+                   [ "Budapest", "Hungary", "BUD" ],
+                   [ "Stuttgart", "Germany", "STR" ],
+                   [ "Nurenberg", "Germany", "NUE" ],
+                   [ "Munich", "Germany", "MUC" ],
+                   [ "Frankfurt", "Germany", "FRA" ],
+                   [ "London Heathrow", "England", "LHR" ],
+                   [ "Berlin", "Germany", "TXL" ],
+                   [ "Toulouse", "France", "TLS" ],
+                   [ "Strasbourg", "France", "SXB" ],
+                   [ "Paris", "France", "CDG" ],
+                   [ "Nice", "France", "NCE" ],
+                   [ "Helsinki", "Finland", "HEL" ],
+                   [ "Dusseldorf", "Germany", "DUS" ],
+                   [ "Cologne", "Germany", "CGN" ],                   
+                   [ "Shannon", "Ireland", "SNN" ],
+                   [ "Dublin", "Ireland", "DUB" ],                   
+                   [ "Manchester", "England", "MAN" ],
+                   [ "London Gatwick", "England", "LGW" ],
+                   [ "Turin", "Italy", "TRN" ],
+                   [ "Gothenburg", "Sweden", "GOT"],
+                   [ "Basel", "Switzerland", "BSL"],
+                   [ "Malaga", "Spain", "AGP" ]
+                  ]
+
+  extras = [
+            [ "Larnaca", "Cyprus", "LNA" ],
+            [ "Riga", "Latvia", "RIX" ],
+           ]
+
+  airport_codes.each do |current_location|
+    flight_search = KayakFlightSearch.new( 'XlTLRouSZWMUuk0NlXRjMw',
+                                           'n',
+                                           'LAX',
+                                           current_location.last,
+                                           start_date,
+                                           end_date,
+                                           num_travelers.to_i )
+
+    flight_search.flight_search( true )
+    cost, url, whole_elements = *flight_search.cheapest_flight
+    puts "#{cost}, #{current_location[1]}, #{current_location[2]}, #{url}"
+  end
   
-  flight_search = KayakFlightSearch.new( 'XlTLRouSZWMUuk0NlXRjMw',
-                                         'n',
-                                         'LAX',
-                                         'LHR',
-                                         start_date,
-                                         end_date,
-                                         num_travelers.to_i )
-
-  flight_search.flight_search( false )
-
-  cost, url, whole_elements = *flight_search.cheapest_flight
-
-  puts "Lowest cost: #{cost}, #{url}"
+  
 end
 
